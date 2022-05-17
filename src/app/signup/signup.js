23ad80859d8599676e6563f6da05d013d49eeba9
Galina Bilonog <galina.bilonog@ab-soft.net>
2022-05-17 08:00:26 +0300

    added body-parser

--- src/app/signup/signup.js
+++ src/app/signup/signup.js
@@ -3,8 +3,11 @@ const signUpRouter = new Router();
 const { signupValidation } = require('../../validation');
 const cookieParser = require('cookie-parser');
 const { itemData } = require('../../../services');
+const BodyParser = require('body-parser');
 
 signUpRouter.use(cookieParser());
+signUpRouter.use(BodyParser.json());
+signUpRouter.use(BodyParser.urlencoded());
 
 signUpRouter.get('/signup', (req, res) => {
     res.render('signup', { error: ''})
@@ -14,20 +17,24 @@ signUpRouter.post('/signup', signupValidation.appValidator, (req, res) => {
     console.log('POST /signup');
     console.log('req.validation',req.validation);
     const { error } = req.validation || '';
-    if(req.validation.error) {
+    console.log('error in post', error);
+    console.log('!!req.validation.error', !!req.validation.error);
+    if(!!req.validation.error) {
         console.log('error', error);
         res.render('signup', { error });
     } else {
         console.log('signup');
         let body = '';
-        req.on('data', data => {
+        req.on('data', async (data) => {
             body += data;
+            console.log('data in post', data);
         })
-    
         req.on('end', async () => {
+            console.log('userName in post');
             const splitedBody = body.split('&');
             const userName = splitedBody[0].replace('userName=', '');
             const password = splitedBody[1].replace('password=', '');
+            
             await itemData.setItem({
                 login: userName,
                 password: password
--- src/validation/signupValidation.js
+++ src/validation/signupValidation.js
@@ -1,10 +1,11 @@
 const Joi = require('joi');
 const { celebrate, Segments } = require('celebrate');
+
 const scheme =  Joi.object({
     //email:joi.string().email().required(),
     userName:Joi.string().min(2).max(25).required(),
     //birthday:joi.date(),
-    password:Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{7,30}$')),
+    password:Joi.string().pattern(new RegExp('[a-zA-Z0-9]$')),
     //confirmPassword:joi.ref('password')
 }); 
 const apiValidator = celebrate({
@@ -13,6 +14,7 @@ const apiValidator = celebrate({
 const appValidator = (req, res, next) => {
     //console.log('req in validator', req);
     const { body } = req;
+    //console.log('body in validator', body);
     req.validation = scheme.validate(body);
     console.log('req.validation in validator', req.validation);
     next();  
